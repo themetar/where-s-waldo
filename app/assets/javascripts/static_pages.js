67a58c04b55ca412641137a5b35abeaa5789ab88
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-08-30 12:29:47 +0200

    Add scene selection list

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener("turbolinks:load", () => {
+  let btn = document.getElementsByTagName("button")[0];
+  btn && btn.addEventListener("click", event => {
+    document.getElementsByClassName("scenes-list")[0].classList.remove("hidden");
+  });
+});
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,5 @@
 class StaticPagesController < ApplicationController
+  def home
+    @scenes = Scene.all
+  end
 end
