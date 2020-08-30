cd42966f65c168ef28849d8e15fe0699731dbb97
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-08-30 21:55:45 +0200

    Fix PlayController test

--- test/controllers/play_controller_test.rb
+++ test/controllers/play_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class PlayControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get play_show_url
+  test "should get play scene" do
+    get play_url(scenes(:beach))
     assert_response :success
   end
 
