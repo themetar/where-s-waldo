0f67d98c786e7af8acd8c96029582aabf6ca3f50
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-09-01 22:12:24 +0200

    Add '/guess' route to PlayController

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,5 +1,16 @@
 class PlayController < ApplicationController
   def show
     @scene = Scene.find(params[:id])
+    session[:game_data] = {}
+  end
+
+  def guess
+    respond_to do |format|
+      count = session[:game_data]["guess"]
+      format.json do
+        session[:game_data] = {}
+        render json: {}
+      end
+    end
   end
 end
--- test/controllers/play_controller_test.rb
+++ test/controllers/play_controller_test.rb
@@ -4,6 +4,14 @@ class PlayControllerTest < ActionDispatch::IntegrationTest
   test "should get play scene" do
     get play_url(scenes(:beach))
     assert_response :success
+    assert session[:game_data]
   end
 
+  test "should make a guess" do
+    get play_url(scenes(:beach))
+    post guess_play_path(scenes(:beach)), xhr: true, params: {}
+    assert_response :success
+    assert_equal "application/json", @response.media_type
+    assert session[:game_data]
+  end
 end
