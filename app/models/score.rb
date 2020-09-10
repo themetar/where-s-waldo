d92a71957024883d8311ddf13e575ba2bade1e94
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-09-10 18:21:59 +0200

    Add Score validation

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,6 @@
 class Score < ApplicationRecord
   belongs_to :scene
+  validates :player_name, presence: true,
+                          length: { minimum: 1, maximum: 140 }
+  validates :time, presence: true
 end
--- test/models/score_test.rb
+++ test/models/score_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ScoreTest < ActiveSupport::TestCase
+  def setup
+    @scene = scenes(:beach)
+  end
+
   test "should get scene association" do
     assert_equal scenes(:beach), scores(:one).scene
+  end
+
+  test "should validate presence of time" do
+    assert @scene.scores.build(player_name: "Test", time: 123).valid?
+    assert @scene.scores.build(player_name: "Test").invalid?
+  end
+
+  test "should validate player name presenece and length" do
+    assert @scene.scores.build(player_name: "Test", time: 123).valid?
+    assert @scene.scores.build(time: 123).invalid?
+    assert @scene.scores.build(player_name: "a"*140, time: 123).valid?
+    assert @scene.scores.build(player_name: "a"*141, time: 123).invalid?
   end
 end
