4bf5a9c90f4c2cd20e80a5a5f6a8004d78e9a9da
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-09-12 19:27:53 +0200

    Rephrase seed file for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Scene.destroy_all
-
-beach = Scene.create({title: "On the beach", asset_name: "waldo-1.jpg"})
-beach.character_locations.create({character: :odlaw,  x: 258,   y: 534})
-beach.character_locations.create({character: :wizard, x: 684,   y: 532})
-beach.character_locations.create({character: :waldo,  x: 1598,  y: 574})
-beach.character_locations.create({character: :wenda,  x: 2004,  y: 627})
+unless Scene.find_by(asset_name: "waldo-1.jpg")
+  beach = Scene.create({title: "On the beach", asset_name: "waldo-1.jpg"})
+  beach.character_locations.create({character: :odlaw,  x: 258,   y: 534})
+  beach.character_locations.create({character: :wizard, x: 684,   y: 532})
+  beach.character_locations.create({character: :waldo,  x: 1598,  y: 574})
+  beach.character_locations.create({character: :wenda,  x: 2004,  y: 627})
+end
